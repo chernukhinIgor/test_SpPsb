0885d3ceea252a658807f8c733403e32311a586e
Georgiy <venom369@yandex.ru>
2017-07-24 15:08:02 +0300

    Changes

--- src/main/java/hello/controller/RestUserController.java
+++ src/main/java/hello/controller/RestUserController.java
@@ -1,5 +1,6 @@
 package hello.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import hello.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -11,7 +12,10 @@ import org.springframework.web.bind.annotation.*;
 import hello.model.User;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -20,9 +24,29 @@ public class RestUserController {
 	private UserService userService;
 
     @GetMapping("user/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable("id") Integer id) {
+    public JsonObject getUserById(@PathVariable("id") Integer id) {
         User user = userService.getUserById(id);
-        return new ResponseEntity<>(user, HttpStatus.OK);
+
+        JsonObject value = Json.createObjectBuilder()
+//                .add("firstName", "John")
+//                .add("lastName", "Smith")
+//                .add("age", 25)
+                .add("data", Json.createObjectBuilder()
+                        .add("userName", user.getName())
+                        .add("city", "New York")
+                        .add("state", "NY")
+                        .add("postalCode", "10021"))
+//                .add("phoneNumber", Json.createArrayBuilder()
+//                        .add(Json.createObjectBuilder()
+//                                .add("type", "home")
+//                                .add("number", "212 555-1234"))
+//                        .add(Json.createObjectBuilder()
+//                                .add("type", "fax")
+//                                .add("number", "646 555-4567")))
+                .build();
+
+        // new ResponseEntity<>(user, HttpStatus.OK);
+        return value;
     }
 
     @GetMapping("users")
