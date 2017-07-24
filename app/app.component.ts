98231a2906a670e068ac89971beabc91df900622
KIRILL <galushko.ka@gmail.com>
2017-07-24 15:34:34 +0300

    Add tasks component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `<div>
-        <h1>Маршрутизация в Angular 2</h1>
         <router-outlet></router-outlet>
     </div>`
 })
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {Routes, RouterModule} from '@angular/router';
-
+import { Routes, RouterModule } from '@angular/router';
+import { HttpModule }   from '@angular/http';
 import { AppComponent }   from './app.component';
-import { AboutComponent }   from './about.component';
 import { HomeComponent }   from './home/home.component';
 import { NotFoundComponent }   from './not-found.component';
+import { TasksComponent }   from './tasks/tasks';
+
+
 
 // определение маршрутов
 const appRoutes: Routes =[
-    { path: '', component: HomeComponent},
+    { path: '', component: HomeComponent },
+    { path: 'tasks', component: TasksComponent },
     { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
-    imports:      [ BrowserModule, RouterModule.forRoot(appRoutes)],
-    declarations: [ AppComponent, HomeComponent, AboutComponent, NotFoundComponent],
+    imports:      [ BrowserModule, RouterModule.forRoot(appRoutes), HttpModule],
+    declarations: [ AppComponent, HomeComponent, TasksComponent, NotFoundComponent],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
