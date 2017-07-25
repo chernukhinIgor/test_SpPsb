655859733e648feea36d53743a14c73423ff4261
Georgiy <venom369@yandex.ru>
2017-07-25 15:04:56 +0300

    Changes

--- src/main/java/hello/controller/RestUserController.java
+++ src/main/java/hello/controller/RestUserController.java
@@ -41,6 +41,7 @@ public class RestUserController {
 
     @GetMapping("users")
     public JSONObject getAllUsers(
+
             @RequestParam(required = false) boolean userId,
             @RequestParam(required = false) boolean name,
             @RequestParam(required = false) boolean surname,
