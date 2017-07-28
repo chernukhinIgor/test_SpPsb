fe2e2690381827aa19216c30a37e2ebde1c0d55c
kirill-galushko <galushko.ka@gmail.com>
2017-07-28 16:00:54 +0300

    Headers for requests

--- app/http.service.ts
+++ app/http.service.ts
@@ -1,25 +1,34 @@
-import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 
 @Injectable()
 export class HttpService{
 
-    constructor(private http: Http){ }
-    getData(url: string, params: URLSearchParams) {
-        var jsonObject = this.http.get(apiUrl + url, params);
+    constructor(private http: Http) {
+    }
+
+    initHeader(){
+        var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        var token = currentUser && currentUser.token;
+        return { headers: new Headers({'Authorization': token}) };
+    }
+
+    getData(url: string, params: any) {
+        console.log(this.initHeader());
+        var jsonObject = this.http.get(apiUrl + url, this.initHeader());
         return jsonObject;
     }
 
     postData(url: string, params: any) {
-        return this.http.post(apiUrl + url, params);
+        return this.http.post(apiUrl + url, params, this.initHeader());
     }
 
 
     putData(url: string, params: any) {
-        return this.http.put(apiUrl + url, params);
+        return this.http.put(apiUrl + url, params, this.initHeader());
     }
 
     deleteData(url: string, params: any) {
-        return this.http.delete(apiUrl + url, params);
+        return this.http.delete(apiUrl + url, this.initHeader());
     }
 }
