b91501fc463ad8ebc5c04839d3028ddc7a12624e
igor.chernukhin <igor.chernukhin@salesplatform.ru>
2017-07-31 15:24:51 +0300

    change headers

--- app/http.service.ts
+++ app/http.service.ts
@@ -3,32 +3,30 @@ import { Http, Headers } from '@angular/http';
 
 @Injectable()
 export class HttpService{
+    private headers: Headers;
 
     constructor(private http: Http) {
-    }
-
-    initHeader(){
         var currentUser = JSON.parse(localStorage.getItem('currentUser'));
         var token = currentUser && currentUser.token;
-        return { headers: new Headers({'Authorization': token}) };
+        this.headers = new Headers({'Authorization': token, 'Content-Type': 'application/json'});
     }
 
+
     getData(url: string, params: any) {
-        console.log(this.initHeader());
-        var jsonObject = this.http.get(apiUrl + url, this.initHeader());
+        var jsonObject = this.http.get(apiUrl + url, this.headers);
         return jsonObject;
     }
 
     postData(url: string, params: any) {
-        return this.http.post(apiUrl + url, params, this.initHeader());
+        return this.http.post(apiUrl + url, params, this.headers);
     }
 
 
     putData(url: string, params: any) {
-        return this.http.put(apiUrl + url, params, this.initHeader());
+        return this.http.put(apiUrl + url, params, this.headers);
     }
 
     deleteData(url: string, params: any) {
-        return this.http.delete(apiUrl + url, this.initHeader());
+        return this.http.delete(apiUrl + url, this.headers);
     }
 }
